f236efdd88317a5ac46789699408fa15d367bb16
Aditya <codecommander03@gmail.com>
2024-06-16 21:45:28 +0530

    easy but took time

---
+++ Codeforces Round 953 (Div. 2)/C_Manhattan_Permutations.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+//					सुखदुःखे समे कृत्वा लाभालाभौ जयाजयौ॥
+//					ततो युद्धाय युज्यस्व नैवं पापमवाप्स्यसि॥
+
+template <typename T, class C = std::less<T>>
+using ordered_set = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, class C = std::less_equal<T>>
+using ordered_multiset = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
+
+struct custom_hash {
+static uint64_t splitmix64(uint64_t x) {
+// http://xorshift.di.unimi.it/splitmix64.c
+x += 0x9e3779b97f4a7c15;
+x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+return x ^ (x >> 31);
+}
+
+size_t operator()(uint64_t x) const {
+static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+return splitmix64(x + FIXED_RANDOM);
+}
+};
+
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             std::set <int>
+#define vi             std::vector <int>
+#define pii            std::pair <int, int>
+#define vpi            std::vector <pii>
+#define vpp            std::vector <pair<int, pii>>
+#define umii           std::unordered_map <int, int, custom_hash>
+#define mii            std::map <int, int>
+#define mpi            std::map <pii, int>
+#define spi            std::set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        std::priority_queue <int>
+#define que_min        std::priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+#define set_bits __builtin_popcountll
+#define lead_zero __builtin_clzll
+#define trail_zero __builtin_ctzll
+
+int nxt() {
+    int x;
+    std::cin >> x;
+    return x;
+}
+
+int pow(int base, int exp) {
+    int res = 1;
+    while (exp > 0) {
+        if (exp & 1)
+            res = res * base;
+        base = base * base;
+        exp /= 2;
+    }
+    return res;
+}
+
+int pow(int base, int exp, int mod) {
+    int res = 1;
+    while (exp > 0) {
+        if (exp & 1)
+            res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+
+const int mod = 1000000007;
+
+void solve(){
+    int n = nxt(), k = nxt();
+
+    if(k&1){
+        cout << "No" << endl;
+        return;
+    }
+    int inv = 0;
+    rep(i, 0, n) inv += abs(i - (n - i - 1));
+
+    if(k > inv){
+        cout << "No" << endl;
+        return;
+    }
+
+    vi v(n);
+    iota(all(v), 1);
+
+    int l = 0, r = n-1;
+    k /= 2;
+    while(k>0 && l<r) {
+        if(k >= r - l){
+            swap(v[l], v[r]);
+            k -= r - l;
+            l++;
+            r--;
+        } else {
+            swap(v[l], v[l+k]);
+            k = 0;
+        }
+    }
+    cout << "Yes" << endl;
+    rep(i, 0, n) cout << v[i] << " \n"[i == n-1];
+}
+
+int32_t main() {
+//#ifndef ONLINE_JUDGE
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+//#endif
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    // std::cout<<std::fixed<<std::setprecision(0);
+
+    int t=1;
+    std::cin>>t;
+    while(t--) solve();
+}
